f30cbcc487a3913bdfa4804a73cd0864a20ff4f6
meatball <69751659+meatball133@users.noreply.github.com>
2023-10-28 19:25:50 +0200

    fix

--- tests/decoders_test.py
+++ tests/decoders_test.py
@@ -1,10 +1,10 @@
 import unittest
 import pytest
-from src import utils
+from src.utils import decode_data_from_pixel
 class PixelDataDecodeTests(unittest.TestCase):
     def test_can_decode_pixel(self):
 
-        result = utils.decode_data_from_pixel((37, 121, 255, 1))
+        result = decode_data_from_pixel((37, 121, 255, 1))
 
         expected = 0b01011101
 
